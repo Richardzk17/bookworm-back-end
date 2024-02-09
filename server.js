3f577c2b3360978e9345baad323ae9604d642c6e
llihwerd <100llihwerd@gmail.com>
2024-02-09 15:30:51 -0500

    import and mount routes

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import './config/database.js'
 // import routes
 import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
+import { router as booksRouter } from './routes/books.js'
 
 // create the express app
 const app = express()
@@ -24,6 +25,7 @@ app.use(formData.parse())
 // mount imported routes
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/books', authRouter)
 
 // handle 404 errors
 app.use(function (req, res, next) {
