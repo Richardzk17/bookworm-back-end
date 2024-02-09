6d321759b0d802734ff1b88be7ad71fd1df3966a
llihwerd <100llihwerd@gmail.com>
2024-02-09 14:36:33 -0500

    fill out book model

---
+++ models/book.js
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const reviewSchema = new Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    enum: ['1', '2', '3', '4', '5']
+  },
+  recommended: {
+    type: Boolean,
+    required: true
+  },
+  // author: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: 'Profile'
+  // }
+}, {
+  timestamps: true
+})
+
+const commentSchema = new Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  // author: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: 'Profile'
+  // }
+}, {
+  timestamps: true
+})
+
+const bookSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    comments: [commentSchema]
+  },
+  { timestamps: true }
+)
+
+const Book = mongoose.model('Book', bookSchema)
+
+export { Book }
