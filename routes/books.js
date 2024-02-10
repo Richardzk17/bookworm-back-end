4af1ef74455f999a78aa94f0b4da7272587dc5c4
BriannaRocha <rochabrianna17@gmail.com>
2024-02-10 16:40:34 -0500

    fix routes/books.js and server.js

--- routes/books.js
+++ routes/books.js
@@ -1,23 +1,20 @@
 import { Router } from "express"
 import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
-import { router } from "./profiles.js"
 import * as booksCtrl from '../controllers/books.js'
 
 const router = Router()
 
 /*---------- Public Routes ----------*/
-
-router.use(decodeUserFromToken)
 router.get('/', checkAuth, booksCtrl.index)
 router.get('/:bookId', checkAuth, booksCtrl.show)
-router.post('/', checkAuth, booksCtrl.create)
-router.post('/:bookId/comments', checkAuth, booksCtrl.createComment)
-router.put('/:bookId', checkAuth, booksCtrl.update)
-router.delete('/:bookId', checkAuth, booksCtrl.delete)
-
-router.post('/:bookId/reviews', checkAuth, booksCtrl.createReviews)
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
+router.post('/', checkAuth, booksCtrl.create)
+router.put('/:bookId', checkAuth, booksCtrl.update)
+router.post('/:bookId/comments', checkAuth, booksCtrl.createComment)
+router.post('/:bookId/reviews', checkAuth, booksCtrl.createReview)
+router.delete('/:bookId/comments/:commentId', checkAuth, blogsCtrl.deleteComment)
+router.delete('/:bookId/reviews/:reviewId', checkAuth, blogsCtrl.deleteReview)
 
 export { router }
