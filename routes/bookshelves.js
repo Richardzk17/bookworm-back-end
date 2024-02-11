84c08abfd5f36f87973293f6b2edc944a972a06f
joshuabutler <butlerja.nhs@gmail.com>
2024-02-10 20:45:36 -0500

    fix up code

--- routes/bookshelves.js
+++ routes/bookshelves.js
@@ -1,6 +1,5 @@
 import { Router } from "express"
 import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
-import { router } from "./profiles.js"
 import * as bookshelvesCtrl from '../controllers/bookshelves.js'
 
 const router = Router()
