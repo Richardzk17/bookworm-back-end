21e894dd417b432bda3f5d0c719113621a0b65ff
llihwerd <100llihwerd@gmail.com>
2024-02-12 16:18:10 -0500

    clean up bookshelves controller

--- controllers/bookshelves.js
+++ controllers/bookshelves.js
@@ -17,30 +17,18 @@ async function create(req, res) {
   }
 }
 
-// async function show(req, res) {
-//   try {
-//     const bookshelf = await Bookshelf.findById(req.params.bookshelfId)
-//       .populate(['myBooks'])
-//     res.status(200).json(bookshelf)
-//   } catch (error) {
-//     console.log(error)
-//     res.status(500).json(error)
-//   }
-// }
-
-async function deleteBook(req, res) {
+async function show(req, res) {
   try {
     const bookshelf = await Bookshelf.findById(req.params.bookshelfId)
-    bookshelf.myBooks.remove({ _id: req.params.bookId })
-    await bookshelf.save()
-    res.status(200).json(blog)
+      .populate(['myBooks'])
+    res.status(200).json(bookshelf)
   } catch (error) {
     console.log(error)
     res.status(500).json(error)
   }
 }
 
+
 export {
-  create,
-  deleteBook as delete
+  create
 }
