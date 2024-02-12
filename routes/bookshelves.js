7461037d298873828297311820133426c56d7538
llihwerd <100llihwerd@gmail.com>
2024-02-12 16:25:51 -0500

    new bookshelves updates

--- routes/bookshelves.js
+++ routes/bookshelves.js
@@ -9,9 +9,6 @@ const router = Router()
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
-router.post('/', checkAuth, booksCtrl.create)
-router.get('/:bookshelfId', checkAuth, bookshelvesCtrl.show) 
-router.put('/:bookshelfId/:bookId', checkAuth, bookshelvesCtrl.update)
-router.delete('/:bookshelfId/:bookId', checkAuth, bookshelvesCtrl.delete)
+router.post('/', checkAuth, bookshelvesCtrl.create)
 
 export { router }
