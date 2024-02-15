327e6029cec6abebc411fd0415f0ab59f71305f9
joshuabutler <butlerja.nhs@gmail.com>
2024-02-15 15:21:07 -0500

    clean up code

--- models/profile.js
+++ models/profile.js
@@ -5,10 +5,6 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   photo: String,
-  // photo: {
-  //   type: String,
-  //   default: 'https://imgur.com/kJnQN7i'
-  // },
   bookshelf: [{ type: Schema.Types.ObjectId, ref: 'Bookshelf' }]
 },{
   timestamps: true,
