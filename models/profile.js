8d17f5cbd0c18716198c3826161407ca260a3315
joshuabutler <butlerja.nhs@gmail.com>
2024-02-15 14:54:30 -0500

    edit model for profile

--- models/profile.js
+++ models/profile.js
@@ -5,6 +5,10 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   photo: String,
+  // photo: {
+  //   type: String,
+  //   default: 'https://imgur.com/kJnQN7i'
+  // },
   bookshelf: [{ type: Schema.Types.ObjectId, ref: 'Bookshelf' }]
 },{
   timestamps: true,
